64b3481cb4f5445d8b4160f182c13fa6bc2a1d20
winily <18934086807@163.com>
2020-02-05 16:54:57 +0800

    bpmn demo

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # bpmn-js-demo
 
+
+Vue 整合 Bpmn-js的一个demo
+详细情况可以去我的博客看<a href="https://winily.github.io/2020/02/03/bpmn-js/#Vue-%E6%95%B4%E5%90%88Bpmn-js">winily的博客</a>
+
+
 ## Project setup
 ```
 npm install
--- src/main.js
+++ src/main.js
@@ -2,6 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// bpmn 相关依赖
+import 'bpmn-js/dist/assets/diagram-js.css' 
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn.css'
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-codes.css'
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css'
+
+// 左边工具栏以及编辑节点的样式
+import 'bpmn-js-properties-panel/dist/assets/bpmn-js-properties-panel.css'
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
 Vue.config.productionTip = false
 
 new Vue({
